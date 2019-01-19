47ca511f7f7cdba05bc30163bae2860efc4f8501
yq <1392227049@qq.com>
2019-01-19 11:53:03 +0800

    more

--- scroll.js
+++ scroll.js
@@ -1,44 +1,59 @@
 class betterScroll {
     //需要传入速度
-    constructor(timer) {
-        this.timer = timer || 10;
+    constructor(el, obj) {
+        this.timer = obj.speed || 10;
         this.top = document.body.scrollTop || document.documentElement.scrollTop;
+        if (el === window) this.el = window;
+        else if (el.charAt(0) === '.') {
+            this.el = document.getElementsByClassName(el.substr(1))[0];
+        }
+        else {
+            this.el = document.getElementById(el.substr(1));
+        }
+        console.log(this.el)
     }
     //移动功能函数
     goTop(top) {
-        window.scrollTo(0, top)
+        this.el.scrollTo(0, top)
     }
     //去相应位置
     toTop(wantTop) {
         wantTop = wantTop || 0
         let i = 0;
-        if(wantTop > this.top) {
+        let nowTops = this.elOrwind();
+        if (wantTop > nowTops) {
             window.timers = setInterval(() => {
-                    this.top += (++i);
-                    this.goTop(this.top);
-                    if (wantTop <= this.top) this.clear()
+                nowTops += i>20? i : ++i;
+                this.goTop(nowTops);
+                if (wantTop <= nowTops) this.clear()
             }, this.timer)
         }
-        else {
+        else if (wantTop < nowTops) {
             window.timers = setInterval(() => {
-                    this.top -= (++i);
-                    this.goTop(this.top);
-                    if (wantTop >= this.top) this.clear()
+                nowTops -= (++i);
+                this.goTop(nowTops);
+                if (wantTop >= nowTops) this.clear()
             }, this.timer)
         }
-        
+        else { }
     }
     //测试scrool的高度
     getScroll() {
-        window.addEventListener('scroll', () => { this.getHeight() }, false)
+        this.el.addEventListener('scroll', () => { this.getHeight() }, false)
     }
     //读取测试高度
     getHeight() {
         console.log(document.body.scrollTop || document.documentElement.scrollTop)
     }
-    //读取当前高度
-    getNowTop() {
+    //读取实例时的高度
+    getNewTop() {
         console.log(this.top)
+    }
+    //获取节点或window对象当前scroll高度
+    elOrwind() {
+        return this.el === window ?
+            document.body.scrollTop || document.documentElement.scrollTop :
+            this.el.scrollTop
     }
     //清除事件
     clear() {
